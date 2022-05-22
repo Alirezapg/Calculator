7dcbadbd0c7b1b87dc23f60250dab6495265c965
First1 <96675836+Alirezapg@users.noreply.github.com>
2022-05-22 12:22:33 -0700

    اضافه کردن اعداد 0 تا 9 و اضافه کردن عملگرا های ریاضی و پاک کردن و دلیت اضافه شدن اعداد 0تا9 و دلیت و پاک کردن . اضافه کردن عملگرا ها مانند تقسیم.ضرب.مثبت.منفی.اعشار.مساوی

---
+++ src/components/Calculator.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <table id="calculator" dir="ltr" style="text-align: center; margin: auto">
+      <tr>
+        <!-- table row -->
+        <td colspan="5">
+          <!-- span = امتداد داشتن -->
+          <input
+            v-model="screen"
+            dir="ltr"
+            id="result"
+            type="number"
+            style="padding: 5px; font-weight: bold; text-align: right"
+          />
+        </td>
+        <!-- table data -->
+      </tr>
+      <tr>
+        <td><button @click="digit(7)" class="digit">7</button></td>
+        <td><button @click="digit(8)" class="digit">8</button></td>
+        <td><button @click="digit(9)" class="digit">9</button></td>
+        <td><button @click="div" id="div">/</button></td>
+        <td><button @click="clear" id="clear">C</button></td>
+      </tr>
+      <tr>
+        <td><button @click="digit(4)" class="digit">4</button></td>
+        <td><button @click="digit(5)" class="digit">5</button></td>
+        <td><button @click="digit(6)" class="digit">6</button></td>
+        <td><button @click="mul" id="mul">*</button></td>
+        <td><button @click="del" id="del">del</button></td>
+      </tr>
+      <tr>
+        <td><button @click="digit(1)" class="digit">1</button></td>
+        <td><button @click="digit(2)" class="digit">2</button></td>
+        <td><button @click="digit(3)" class="digit">3</button></td>
+        <td><button @click="minus" id="minus">-</button></td>
+        <td><button @click="mod" id="mod">%</button></td>
+      </tr>
+      <tr>
+        <td colspan="2"><button @click="digit(0)" class="digit">0</button></td>
+        <td><button @click="decimal" class="decimal">.</button></td>
+        <td><button @click="sum" id="sum">+</button></td>
+        <td><button @click="equal" id="equal">=</button></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script> 
+
+<style scoped>
+</style>
